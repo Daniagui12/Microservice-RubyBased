4c815c549de741b05aa484dfdba3761dd605c07b
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-12-03 04:55:06 -0500

    update

---
+++ app/controllers/solicitud_controller.rb
@@ -0,0 +1,10 @@
+class SolicitudsController < ApplicationController
+    def say
+        @message = Cow.new.say(params[:message])
+      end
+  
+      def get_solicitudes
+        @solicitudes = Solicitud.all
+        render json: @solicitudes
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  jsonapi_resources :solicituds
+  get 'solicitud', to: 'solicitud#get_solicitudes'
   # Defines the root path route ("/")
   # root "articles#index"
 end
