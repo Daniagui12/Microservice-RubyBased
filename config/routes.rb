361ca79daece9ca2a47c0b56265e6f7d9a0dd5d2
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-12-03 04:08:20 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  jsonapi resources :solicituds
+  jsonapi_resources :restaurants
   # Defines the root path route ("/")
   # root "articles#index"
 end
