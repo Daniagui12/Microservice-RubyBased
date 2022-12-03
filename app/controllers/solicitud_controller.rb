98ba043b5329634aa7813dc60f6087cac93bec1a
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-12-03 04:01:26 -0500

    fixing

--- app/controllers/solicitud_controller.rb
+++
@@ -1,3 +0,0 @@
-class SolicitudeController < JSONAPI::ResourceController
-  skip_before_action :verify_authenticity_token
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  jsonapi_resources :solicitudes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  jsonapi_resources :solicituds
   # Defines the root path route ("/")
   # root "articles#index"
 end
