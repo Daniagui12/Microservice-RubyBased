3cba70d08745ee630ca5a42936e14f11a75b3021
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-12-05 02:30:11 -0500

    Update solicitud_controller.rb

--- app/controllers/solicitud_controller.rb
+++ app/controllers/solicitud_controller.rb
@@ -10,9 +10,9 @@ class SolicitudController < ApplicationController
 
       def get_solicitud_by_empresa_empleado
         @Empleados = Empleado.where(empresaAfiliada_id: params[:empresa_id])
-        puts @Empleados
         listIds = []
         @Empleados.each do |empleado|
+            puts empleado.id
             listIds.push(empleado.id)
         end
         puts listIds
