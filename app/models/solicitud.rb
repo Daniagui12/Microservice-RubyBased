cc264582cc6bfe06157fbfb3a946bf13172bc57d
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-12-05 03:03:29 -0500

    Update solicitud.rb

--- app/models/solicitud.rb
+++ app/models/solicitud.rb
@@ -1,4 +1,9 @@
 class Solicitud
     include Mongoid::Document
     store_in collection: "solicitud"
+    field :id_solicitud, type: Integer
+    field :id_cliente_id, type: Integer
+    field :fechaCreacion, type: Date
+    field :estado , type: String
+    field :documentoCliente , type: String
 end
